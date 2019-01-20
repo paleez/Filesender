a79a0bc05102cc4ab51350cde137891663d9cfd4
Dennis <bubblan.dn@gmail.com>
2019-01-20 01:20:10 +0100

    Initial commit

---
+++ Filesender/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Filesender
+{
+    class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action execute;
+
+        public Command(Action execute)
+        {
+            this.execute = execute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
---
+++ Filesender/MainWindowViewModel.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Filesender
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand ChooseFolderCommand { get; }
+        public ICommand ConnectCommand { get; }
+        public ICommand SetMyPortCommand { get; }
+        public ICommand StartMyServerCommand { get; }
+        public ICommand SendFileCommand { get; }
+
+        public int MyPort { get { return myPort; } set { myPort = value; OnPropertyChanged(nameof(MyPort)); }  }
+        private int myPort;
+        public string TheirIp { get { return theirIp; } set { theirIp = value; OnPropertyChanged(nameof(TheirIp)); } }
+        private string theirIp;
+        public int TheirPort { get { return theirPort; } set { theirPort = value; OnPropertyChanged(nameof(TheirPort)); } }
+        private int theirPort;
+        private string myFolder = "C:\\";
+        private string fileToSendPath; //mebe
+
+        public string IsConnected { get { return isConnected; } set { isConnected = value; OnPropertyChanged(nameof(IsConnected)); } }
+        private string isConnected;
+        Thread listeningThread, connectingThread;
+        TcpListener tcp;
+        //string filePath, string hostName, int port
+
+        public MainWindowViewModel()
+        {
+            ChooseFolderCommand = new Command(ChooseFolder);
+            ConnectCommand = new Command(Connect);
+            SendFileCommand = new Command(SendFile);
+            StartMyServerCommand = new Command(StartMyServer);
+            listeningThread = new Thread(Listen);
+            connectingThread = new Thread(ConnectThread);
+        }
+
+        private void ChooseFolder()
+        {
+            var dialog = new CommonOpenFileDialog()
+            {
+                Title = "Select Folder",
+                IsFolderPicker = true,
+                AddToMostRecentlyUsedList = false,
+                AllowNonFileSystemItems = false,
+                EnsureFileExists = true,
+                EnsurePathExists = true,
+                EnsureReadOnly = false,
+                EnsureValidNames = true,
+                Multiselect = false,
+                ShowPlacesList = true
+            };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                myFolder = dialog.FileName;
+            }
+        }
+
+        private void Connect()
+        {
+            connectingThread.Start();
+        }
+        private void ConnectThread()
+        {
+            TcpClient tc = new TcpClient();
+            Console.WriteLine(IPAddress.Parse(theirIp));
+            tc.Connect(IPAddress.Parse(theirIp), 21);
+        }
+        private void StartMyServer()
+        {
+            IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = ipEntry.AddressList[1];
+            TcpListener tcpListener = new TcpListener(ip, myPort);
+            tcp = tcpListener;
+            tcpListener.Start();
+            Console.WriteLine("this is ip " + ip);
+            Console.WriteLine("this is port " + myPort);
+            listeningThread.Start();
+        }
+        private void Listen()
+        {
+            Socket s = tcp.AcceptSocket();
+        }
+        private void SendFile()
+        {
+
+        }
+        public void SendTCP(string M, string IPA, Int32 PortN)
+        {
+            byte[] SendingBuffer = null;
+            TcpClient client = null;
+            NetworkStream netstream = null;
+            int BufferSize = 1024;
+            try
+            {
+                //client = new TcpClient(IPA, PortN);
+                isConnected = "Connected to the Server...\n";
+                netstream = client.GetStream();
+                FileStream Fs = new FileStream(M, FileMode.Open, FileAccess.Read);
+                
+                int NoOfPackets = Convert.ToInt32
+             (Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(BufferSize)));
+                //progressBar1.Maximum = NoOfPackets;
+                int TotalLength = (int)Fs.Length, CurrentPacketLength, counter = 0;
+                for (int i = 0; i < NoOfPackets; i++)
+                {
+                    if (TotalLength > BufferSize)
+                    {
+                        CurrentPacketLength = BufferSize;
+                        TotalLength = TotalLength - CurrentPacketLength;
+                    }
+                    else
+                        CurrentPacketLength = TotalLength;
+                    SendingBuffer = new byte[CurrentPacketLength];
+                    Fs.Read(SendingBuffer, 0, CurrentPacketLength);
+                    netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+                    //if (progressBar1.Value >= progressBar1.Maximum)
+                      //  progressBar1.Value = progressBar1.Minimum;
+                   // progressBar1.PerformStep();
+                }
+
+                //lblStatus.Text = lblStatus.Text + "Sent " + Fs.Length.ToString() + " 
+        
+                        //bytes to the server";
+                     Fs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                netstream.Close();
+                client.Close();
+            }
+        }
+    }
+}
